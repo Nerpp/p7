9d3599167f2649f7a41dc15d5b0bf617262943de
Nerpp <regiaurelien@gmail.com>
2021-05-29 14:41:18 +0200

    entity with relation setted && fixture loaded

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -73,6 +73,11 @@ class Product
      */
     private $user;
 
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -206,6 +211,18 @@ class Product
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
 
         return $this;
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -51,6 +51,11 @@ class User implements UserInterface
      */
     private $customer;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
     public function __construct()
     {
         $this->product = new ArrayCollection();
@@ -206,6 +211,18 @@ class User implements UserInterface
                 $customer->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
 
         return $this;
     }
